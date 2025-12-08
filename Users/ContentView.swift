f645ca16f5de5eef39a20ea08970819786167615
nmadariaga <neftali.madariaga@gmail.com>
2025-12-08 11:18:57 -0300

    Initial Commit

---
+++ Users/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Users
+//
+//  Created by Neftalí Madariaga Castro on 08-12-25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Users/UsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsersApp.swift
+//  Users
+//
+//  Created by Neftalí Madariaga Castro on 08-12-25.
+//
+
+import SwiftUI
+
+@main
+struct UsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
